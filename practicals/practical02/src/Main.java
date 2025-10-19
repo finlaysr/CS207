c9168029393884685ee4e1679287931b9f18acc0
finlaysr <60317020+finlaysr@users.noreply.github.com>
2025-10-19 11:45:07 +0100

    Finished question 2 from practical 2

--- practicals/practical02/src/Main.java
+++ practicals/practical02/src/Main.java
@@ -1,16 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the
-  // highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have
-    // set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut
-    // actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+class Main {
+  public static void main(String[] args) {
+    System.out.println(String.format("Hello and welcome!"));
+    question2();
+  }
+
+  public static void question2() {
+    StackADT<String> stackADT = new LinkedStack<>();
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Please type one option. Values in [] are required, values in () are not.");
+    boolean done = false;
+
+    while (!done) {
+      System.out.print("\nOptions: add [e], remove (top), show (top), empty, size, traverse, quit: ");
+
+      String option = scanner.nextLine().trim().toLowerCase();
+      String param = "";
+      if (option.contains(" ")) {
+        param = option.split(" ")[1];
+        option = option.split(" ")[0];
+      }
+
+      switch (option) {
+        case "add": {
+          if (param.isEmpty()) {
+            System.out.println("Please input the value you want to add, e.g. add hello");
+          } else {
+            stackADT.push(param);
+            System.out.println("Added " + param + " to the stack");
+          }
+          break;
+        }
+
+        case "remove": {
+          try {
+            String removed = stackADT.pop();
+            System.out.println("Removed: " + removed);
+          } catch (EmptyStackException e) {
+            System.out.println("No elements to remove!");
+          }
+          break;
+        }
+
+        case "show": {
+          try {
+            System.out.println("Top: " + stackADT.getTop());
+          } catch (EmptyStackException e) {
+            System.out.println("No elements to show!");
+          }
+          break;
+        }
+
+        case "empty": {
+          System.out.println("The stack is " + (stackADT.isEmpty() ? "" : "not ") + "empty");
+          break;
+        }
+
+        case "size": {
+          System.out.println("Size: " + stackADT.size());
+          break;
+        }
+
+        case "traverse": {
+          try {
+            stackADT.traverse();
+          } catch (EmptyStackException e) {
+            System.out.println("No elements to traverse!");
+          }
+          break;
+        }
+
+        case "quit": {
+          done = true;
+          break;
+        }
+
+        default: {
+          System.out.println("Invalid option");
+          break;
+        }
+
+      }
+    }
+    scanner.close();
   }
 }
